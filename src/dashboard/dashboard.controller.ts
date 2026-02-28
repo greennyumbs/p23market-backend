c9482e34bbd2cdfa2fe72b68a66a8648d0020f7f
greennyumbs <thanatat.sinc@hotmail.com>
2026-02-28 18:32:34 +0700

    removes JWT auth from dashboard

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,12 +1,9 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('v1/dashboard')
-@ApiBearerAuth()
 @Controller('api/v1/dashboard')
-@UseGuards(JwtAuthGuard)
 export class DashboardController {
   constructor(private dashboardService: DashboardService) {}
 
